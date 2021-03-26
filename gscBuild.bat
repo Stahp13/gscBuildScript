86762dbba9e633d818638b0c9d58049a2506f43e
Stanislaw Morawski <stasanko@gmail.com>
2021-03-27 00:37:32 +0100

    initial commit for buildscript

---
+++ gscBuild.bat
@@ -0,0 +1,44 @@
+@echo off & setlocal enabledelayedexpansion
+cd %1
+setlocal
+
+for %%I in (.)  do (
+    set dir_name=%%~nxI
+)
+if not %dir_name%==maps (
+    echo "root name for compilation has to be called 'maps'"
+    exit 1
+)
+if "%t6r_data%"=="" (
+    echo "enviromental variable t6r_data does not exist"
+    exit 1
+)
+if not exist %t6r_data% (
+    echo "enviromental variable t6r_data does not point to directory"
+    exit 1
+)
+
+set deploy_path=%t6r_data%\maps
+set deploy_path=%deploy_path:/=\%
+echo deploy_path = %deploy_path%
+
+rmdir /s/Q %deploy_path%
+mkdir %deploy_path%
+Xcopy /s/e . "%deploy_path%"
+cd %deploy_path%
+
+:compile
+echo %cd_length%
+for %%F in (*.gsc) do (
+    echo %%F
+    gscCompiler.exe %%F
+    del /f %%F
+    ren %%~nF-compiled.gsc %%~nF.gsc
+)
+for /D %%d in (*) do (
+    cd %%d
+    echo CD=%CD%
+    call :compile
+    cd ..
+)
+endlocal
